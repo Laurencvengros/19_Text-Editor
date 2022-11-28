76a273e2942e57e6b38076add26b7e8ca8825f8d
Lauren <l.cvengros@icloud.com>
2022-11-28 16:32:26 -0500

    added logic to clear the prompt when app is installed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,10 +16,12 @@ butInstall.addEventListener('click', async () => {
     }
 
     promptEvent.prompt();
-    
+
     window.deferredPrompt = null;
     butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
